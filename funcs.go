4e740295ec48982977cc5b67c036bd8a56efdd67
ligz <li_gz@suixingpay.com>
2021-07-12 05:33:21 +0800

    mtools: first

---
+++ funcs.go
@@ -0,0 +1,30 @@
+package ctools
+
+import (
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+func GetNowTimeStr() string {
+	return time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
+}
+
+func GetHostName() string {
+	name, _ := os.Hostname()
+	return name
+}
+
+func GetLocalIp() string {
+	conn, err := net.Dial("udp", "8.8.8.8:53")
+	if err != nil {
+		log.Printf("get local addr err:%v", err)
+		return ""
+	} else {
+		localIp := strings.Split(conn.LocalAddr().String(), ":")[0]
+		conn.Close()
+		return localIp
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/guozhengli/ctools
+
+go 1.16
